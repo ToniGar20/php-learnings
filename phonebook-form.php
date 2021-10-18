1f450ee7011a074b917208460cfad9dc114361f8
Toni <garcia.t20@gmail.com>
2021-10-19 01:10:19 +0200

    application functionally working!

--- phonebook-form.php
+++ phonebook-form.php
@@ -16,10 +16,10 @@
      * Definition of the array with key-value structure
      */
 
-    $phonebook = [];
-    $new_name = $_GET["contact"];
-    $new_phone = $_GET["phone"];
-    $phonebook[$new_name] = $new_phone;
+    $phonebook = $_GET["phonebook"] ?? [];
+    $new_contact = filter_input(INPUT_GET, "contact");
+    $new_phone = filter_input(INPUT_GET, "phone");
+    $phonebook[$new_contact] = $new_phone;
 
     /**
      * Validations:
@@ -27,14 +27,15 @@
      * 2. If phone is empty, contact is removed
      * 3. If both filled, contact added
      */
-    if(empty($new_name)){
-        unset($phonebook[$new_name]);
+
+    if(empty($new_contact)){
+        unset($phonebook[$new_contact]);
         echo "<p>Introduce un NOMBRE para añadir un contacto a la agenda</p>";
     } elseif (empty($new_phone)){
-        unset($phonebook[$new_name]);
-        echo "<p>El teléfono de " . $phonebook[$new_name] . "ha sido eliminado</p>";
+        echo "<p>El teléfono de " . $new_contact . " ha sido eliminado</p>";
+        unset($phonebook[$new_contact]);
     } else {
-        $phonebook[$new_name] = [$new_phone];
+        $phonebook[$new_contact] = $new_phone;
     }
 
     // Printing value of phonebook;
@@ -46,14 +47,13 @@
     <p>Añadir o borrar un nuevo contacto</p>
     <form method="get">
         <?php
-        if(count($phonebook) >=1 ){
-            foreach ($phonebook as $name =>$phone){
-                echo '<input type="hidden" name="' . $name . '" value="' . $phone .  '"/>';
+            foreach ($phonebook as $contact => $phone){
+                echo '<input type="hidden" name="phonebook[' . $contact . ']" value="' . $phone .  '"/>';
+
             }
-        }
         ?>
-        <input type="text" name="contact[]" placeholder="Nombre"/>
-        <input type="text" name="phone" placeholder="Teléfono"/>
+        <input type="text" name="contact" placeholder="Nombre"/>
+        <input type="number" name="phone" placeholder="Teléfono"/>
         <input type="submit" name="submit" value="Enviar"/>
     </form>
 
@@ -63,8 +63,8 @@
         /**
          * Iteration to display all contacts
          */
-        foreach ($phonebook as $name => $phone ){
-            echo "<li>" . $name .": ". $phone . "</li>";
+        foreach ($phonebook as $contact => $phone ){
+            echo "<li>" . $contact .": ". $phone . "</li>";
         }
         ?>
     </div>
