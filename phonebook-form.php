064c4409a55c9b17c8b3daddc1837584708e3a01
Toni <garcia.t20@gmail.com>
2021-10-19 09:25:23 +0200

    different features: styles, default message and better validation conditions following practice requirements

--- phonebook-form.php
+++ phonebook-form.php
@@ -6,68 +6,88 @@
     <title>Phonebook Form</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {font-family: Verdana}
+    </style>
 </head>
 <body>
-
     <?php
     /**
-     * Initializing array to store all contacts
-     * Grabbing phone and contacts from form values (GET)
-     * Definition of the array with key-value structure
+     * 1. Initializing array if the array does not exist
+     * 2. Grabbing phone and contacts from form values (GET) with input filter
      */
-
     $phonebook = $_GET["phonebook"] ?? [];
     $new_contact = filter_input(INPUT_GET, "contact");
     $new_phone = filter_input(INPUT_GET, "phone");
-    $phonebook[$new_contact] = $new_phone;
-
-    /**
-     * Validations:
-     * 1. Empty name = no contact added
-     * 2. If phone is empty, contact is removed
-     * 3. If both filled, contact added
-     */
-
-    if(empty($new_contact)){
-        unset($phonebook[$new_contact]);
-        echo "<p>Introduce un NOMBRE para añadir un contacto a la agenda</p>";
-    } elseif (empty($new_phone)){
-        echo "<p>El teléfono de " . $new_contact . " ha sido eliminado</p>";
-        unset($phonebook[$new_contact]);
-    } else {
-        $phonebook[$new_contact] = $new_phone;
-    }
-
-    // Printing value of phonebook;
-    print_r($phonebook);
-
     ?>
 
+    <!-- Headings -->
     <h1>Agenda de contactos</h1>
-    <p>Añadir o borrar un nuevo contacto</p>
+    <p>Añade, actualiza o borra un contacto</p>
+    <?php
+    /**
+     * Validations - With no submit default message. With submit evaluation of conditions for validation:
+     * 1. Empty name = no contact added
+     * 2. Empty phone && filled name = if contact is in the phonebook, contact removed. If contact is not in the phonebook, message displayed
+     * 3. Both inputs filled = if contact is in the phonebook, message of contact updated. If contact is not in the phonebook, added as new
+     */
+    if(isset($_GET["submit"])) {
+        if (empty($new_contact)) {
+            echo "<p style='color:orange'>Introduce un nombre para añadir un contacto.</p>";
+            unset($phonebook[$new_contact]);
+        } elseif (empty($new_phone)) {
+            if (array_key_exists($new_contact, $phonebook)) {
+                echo "<p style='color:darkred'>El teléfono de " . $new_contact . " ha sido eliminado</p>";
+                unset($phonebook[$new_contact]);
+            } else {
+                echo "<p style='color:orange'>El contacto a eliminar no existe en la agenda</p>";
+            }
+        } else {
+            if (array_key_exists($new_contact, $phonebook)) {
+                echo "<p style='color: blue'> El contacto de <b>" . $new_contact . "</b> ha sido actualizado</p>";
+            } else {
+                echo "<p style='color: green'> Has añadido a <b>" . $new_contact . "</b> como nuevo contacto</p>";
+            }
+            $phonebook[$new_contact] = $new_phone;
+        }
+    } else{
+        echo "<p><i>Rellena los datos con un nombre y un teléfono</i></p>";
+    }
+    ?>
+
+    <!-- Form display -->
     <form method="get">
         <?php
+        /**
+         * Adding current contacts of the array as hidden inputs, so everytime the form is submitted
+         * all data is sent again to the array to be displayed in the HTML
+         */
             foreach ($phonebook as $contact => $phone){
                 echo '<input type="hidden" name="phonebook[' . $contact . ']" value="' . $phone .  '"/>';
-
             }
         ?>
         <input type="text" name="contact" placeholder="Nombre"/>
-        <input type="number" name="phone" placeholder="Teléfono"/>
+        <input type="text" name="phone" placeholder="Teléfono"/>
         <input type="submit" name="submit" value="Enviar"/>
     </form>
-
     <div>
+
+        <!-- Contact list -->
         <h2>Listado de contactos en la agenda</h2>
         <?php
         /**
-         * Iteration to display all contacts
+         * Iteration to display all contacts. If the array has no values, message is displayed
          */
-        foreach ($phonebook as $contact => $phone ){
-            echo "<li>" . $contact .": ". $phone . "</li>";
-        }
+        if(count($phonebook) == 0){
+            echo "<p>No hay contactos en la agenda</p>";
+        } else {
+            echo "<ul>";
+            foreach ($phonebook as $contact => $phone ){
+                echo "<li>" . $contact .": ". $phone . "</li>";
+            }
+            echo "</ul>";
+        };
         ?>
     </div>
-
 </body>
 </html>
