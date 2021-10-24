a58e42082dd77c03360cef4d309e310a81745627
Toni <garcia.t20@gmail.com>
2021-10-24 12:56:55 +0200

    buuble agorithm with some html and random number generation

--- tests-and-notes-unit2/bubble-algorithm.php
+++ tests-and-notes-unit2/bubble-algorithm.php
@@ -1,8 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Bubble algorithm</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            body {font-family: Verdana;}
+        </style>
+    </head>
+    <body>
+        <h1>Algoritmo de burbuja</h1>
+    <?php
 
-    //$numbers = [49,24,36,80,31];
-    $numbers = [123,53,-2,44443,4,18,1.5];
+    //Code to generate array with 5 random numbers
+    $numbers = [];
+    while(count($numbers) < 5) {
+        $random_number = rand(-100, 100);
+        array_push($numbers, $random_number);
+    }
 
+    //Printing the random numbers
+    echo "Se han generado 5 números aleatorios entre -100 y 100 que son estos: $numbers[0], $numbers[1], $numbers[2], $numbers[3] y $numbers[4]";
+    echo "<br>";
+    echo "<p><b>Resultado de números ordenados de menor a mayor</b></p>";
+
+    //Algorithm for sort the numbers
     for ($i=0; $i < count($numbers); $i++) {
         for ($j = 0; $j < count($numbers)-1; $j++) {
             if ($numbers[$j] > $numbers[$j+1]) {
@@ -12,7 +36,21 @@
             }
         }
     }
-    print_r($numbers);
-?>
+
+    //Printing sorted numbers, from minor to major
+    echo "<ul>";
+    foreach ($numbers as $number){
+        echo "<li>$number</li>";
+    }
+    echo "</ul>";
+
+    ?>
+    </body>
+</html>
+
+
+
+
+
 
 
