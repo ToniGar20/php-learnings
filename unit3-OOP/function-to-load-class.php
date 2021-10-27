a9d948d1c8d1a166cfc34e93b6d3a6f7099f577e
Toni <garcia.t20@gmail.com>
2021-10-27 17:27:43 +0200

    anonymus spl_autoload, more useful

--- unit3-OOP/function-to-load-class.php
+++ unit3-OOP/function-to-load-class.php
@@ -8,12 +8,11 @@
  * The way above is deprecated at PHP 8.0. The one below is currently the valid way to autoload classes.
  */
 
-
-spl_autoload_register( function ($Person){
-    include "classes/Person.php";
+spl_autoload_register(function ($name){     //$name is equal to the class name
+    include "classes/" . "$name" . ".php";  //$name is concatenated with the folder name to find the file and be able to create the object
 });
 
 $p = new Person("Toni", "García", 33);
-$p->showData();
+$p -> showData();
 
 ?>
