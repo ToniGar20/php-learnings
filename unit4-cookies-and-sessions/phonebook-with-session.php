edfa32348a30f4472c5ff167ef3d2fe38525d6da
Toni <garcia.t20@gmail.com>
2021-11-04 18:58:53 +0100

    new phonebook with sessions instead of hidden inputs

--- unit4-cookies-and-sessions/phonebook-with-session.php
+++ unit4-cookies-and-sessions/phonebook-with-session.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
 <?php
 //Starting the session
 session_start();
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="es">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,21 +15,13 @@ session_start();
     </style>
 </head>
 <body>
-<?php
-/**
- * 1. Initializing array if the array does not exist
- * 2. Grabbing phone and contacts from form values (GET) with input filter
- */
-$phonebook = $_GET["phonebook"] ?? [];
-$new_contact = filter_input(INPUT_GET, "contact");
-$new_phone = filter_input(INPUT_GET, "phone");
-?>
 
-<!-- Headings -->
-<h1>Agenda de contactos</h1>
-<p>Añade, actualiza o borra un contacto</p>
 <?php
+    $new_contact = filter_input(INPUT_GET, "contact");
+    $new_phone = filter_input(INPUT_GET, "phone");
+
 /**
+ * USING $_SESSION for store the values!
  * Validations - With no submit default message. With submit evaluation of conditions for validation:
  * 1. Empty name = no contact added
  * 2. Empty phone && filled name = if contact is in the phonebook, contact removed. If contact is not in the phonebook, message displayed
@@ -38,62 +30,63 @@ $new_phone = filter_input(INPUT_GET, "phone");
 if(isset($_GET["submit"])) {
     if (empty($new_contact)) {
         echo "<p style='color:orange'>Introduce un nombre para añadir un contacto.</p>";
-        unset($phonebook[$new_contact]);
+        unset($_SESSION[$new_contact]);
     } elseif (empty($new_phone)) {
-        if (array_key_exists($new_contact, $phonebook)) {
+        if (array_key_exists($new_contact, $_SESSION)) {
             echo "<p style='color:darkred'>El teléfono de " . $new_contact . " ha sido eliminado</p>";
-            unset($phonebook[$new_contact]);
+            unset($_SESSION[$new_contact]);
         } else {
             echo "<p style='color:orange'>El contacto a eliminar no existe en la agenda</p>";
         }
     } else {
-        if (array_key_exists($new_contact, $phonebook)) {
+        if (array_key_exists($new_contact, $_SESSION)) {
             echo "<p style='color: blue'> El contacto de <b>" . $new_contact . "</b> ha sido actualizado</p>";
         } else {
             echo "<p style='color: green'> Has añadido a <b>" . $new_contact . "</b> como nuevo contacto</p>";
         }
-        $phonebook[$new_contact] = $new_phone;
+        $_SESSION[$new_contact] = $new_phone;
     }
 } else{
     echo "<p><i>Rellena los campos con un nombre y un teléfono</i></p>";
 }
 ?>
 
+    <!-- Headings -->
+    <h1>Agenda de contactos</h1>
+    <p>Añade, actualiza o borra un contacto</p>
 
-<?php
-session_start();
-echo 'session_id(): ' . session_id();
-
-echo "<br />\n";
-echo 'session_name(): ' . session_name();
-echo "<br />\n";
-print_r(session_get_cookie_params());
-?>
-
-<!-- Form display -->
 <form method="get">
     <input type="text" name="contact" placeholder="Nombre"/>
     <input type="text" name="phone" placeholder="Teléfono"/>
     <input type="submit" name="submit" value="Enviar"/>
 </form>
-<div>
 
+<div>
     <!-- Contact list -->
     <h2>Listado de contactos en la agenda</h2>
     <?php
     /**
-     * Iteration to display all contacts. If the array has no values, message is displayed
+     * Iteration to display all contacts. If the $_SESSION has no values, message is displayed
      */
-    if(count($phonebook) == 0){
+    if(count($_SESSION) == 0){
         echo "<p>No hay contactos en la agenda</p>";
     } else {
         echo "<ul>";
-        foreach ($phonebook as $contact => $phone ){
+        foreach ($_SESSION as $contact => $phone ){
             echo "<li>" . $contact .": ". $phone . "</li>";
         }
         echo "</ul>";
     };
     ?>
 </div>
+
+<?php
+/**
+ * Script to display the value of the super-global and also to destroy the current session.
+ */
+//    echo "<br>";
+//    print_r($_SESSION);
+//    //session_destroy();
+//?>
 </body>
 </html>
