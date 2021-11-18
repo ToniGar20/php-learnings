d5219fc85ac28f5a106dfda18238187dd23046a2
Toni <garcia.t20@gmail.com>
2021-11-18 19:10:04 +0100

    doing the first form with file upload!

--- 3-forms/files/uploads-helper.php
+++ 3-forms/files/uploads-helper.php
@@ -3,8 +3,7 @@
  * Documentación del código
  * 1. Comentado el printeo de la variable $_FILES para comprobar que se recibe el archivo desde el formulario
  * 2. $directory almacena el directorio al que se enviarán los archivos.
- * 3. Condicional que evalua si la variable global tiene un valor de array con el "name" especificado en el input
- * 4. Se capturan los valores del array en variables
+ * 3. Condicional que evalua si la variable global tiene un valor de array con el "name" especificado en el input 4. Se capturan los valores del array en variables
  * 5. Se usar el método "move_uploaded_file" para subir el archivo. Basta especificar el nombre temporal y cómo guardarlo, es decir, ruta más nombre
  *
  */
@@ -19,7 +18,8 @@ if(isset($_FILES['fileName'])) {
     $file_size = $_FILES['fileName']['size'];
     $file_tmp = $_FILES['fileName']['tmp_name'];
     $file_type = $_FILES['fileName']['type'];
+
+    move_uploaded_file($file_tmp, $directory . $file_name);
+    echo "Success";
 }
 
-move_uploaded_file($file_tmp, $directory . $file_name);
-echo "Success";
