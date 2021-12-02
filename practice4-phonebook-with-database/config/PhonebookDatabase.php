1e3cccf2e2994e2a052cc20d1a8eade0f300bc6c
Toni <garcia.t20@gmail.com>
2021-12-02 22:14:43 +0100

    database configuration

--- practice4-phonebook-with-database/config/PhonebookDatabase.php
+++ practice4-phonebook-with-database/config/PhonebookDatabase.php
@@ -13,9 +13,15 @@ class PhonebookDatabase
     //Doing connection to database
     public function doConnection(): ?PDO
     {
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
         $this->conn = null;
         try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password, $options);
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
